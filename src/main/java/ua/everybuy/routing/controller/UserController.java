0d7f6e186393b79b89bec52cd51f62235ca78fda
Eugen2510 <Lugovoj20@gmail.com>
2024-09-17 19:26:53 +0200

    added try-catch block in sendInfoAboutChange method  RequestSenderService class

--- src/main/java/ua/everybuy/routing/controller/UserController.java
+++ src/main/java/ua/everybuy/routing/controller/UserController.java
@@ -1,6 +1,5 @@
 package ua.everybuy.routing.controller;
 
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
